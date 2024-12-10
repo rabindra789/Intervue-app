7965451413e1b836b7d6de6b7e9ed3e9de0b2115
rabindra18 <rabindrameher116@gmail.com>
2024-12-10 18:51:56 +0530

    signup/signin completed

---
+++ index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override")
+const connectDB = require("./server/config/db.js");
+const session = require("express-session");
+const passport = require("passport");
+const mongoStore = require("connect-mongo");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+    session({
+      secret: process.env.SECRET_SESSION,
+      resave: false,
+      saveUninitialized: true,
+      store: mongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+      }),
+      cookie: {maxAge: new Date(Date.now() + 604800000)}
+    })
+  );
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"))
+
+// connect database
+connectDB();
+
+app.use(express.static("public"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+  
+
+app.use("/", require("./server/routes/auth.route.js"));
+app.use("/", require("./server/routes/index.route.js"));
+
+
+app.get("*", function (req, res) {
+  res.status(404).render("404");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+});
---
+++ server/controllers/main.controller.js
@@ -0,0 +1,22 @@
+// homepage
+
+exports.homepage = async (req, res) => {
+    const locals = {
+        title: "Intervue App",
+        description: "Open source Intervue app"
+    }
+
+    res.render("index", {locals, layout: "../views/layouts/main"})
+}
+
+
+exports.about = async (req, res) => {
+    const locals = {
+        title: "About - Notes App",
+        description: "Open source notes app"
+    }
+
+    res.render("about", locals)
+}
+
+
